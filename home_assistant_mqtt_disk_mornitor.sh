e2e5f4fbbe60a6ffb861d0e505de414854b75645
kwiatkowskimarcin <kwiatkowskimarcin.mail@gmail.com>
2022-04-05 14:03:13 +0200

    HA mqtt disk monitor

--- home_assistant_mqtt_disk_mornitor.sh
+++ home_assistant_mqtt_disk_mornitor.sh
@@ -8,8 +8,10 @@ size=`df -h $1 | awk '{ print $2 }' | tail -n 1 | cut -d 'G' -f1`
 available=`df -h $1 | awk '{ print $4 }' | tail -n 1 | cut -d 'G' -f1`
 used=`df -h $1 | awk '{ print $3 }' | tail -n 1 | cut -d 'G' -f1`
 used_percentage=`df -h $1 | awk '{ print $5 }' | tail -n 1 | cut -d '%' -f1`
+temp=`/usr/sbin/hddtemp -n $1`
 
 /usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/size" -m "$size"
 /usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/available" -m "$available"
 /usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/used" -m "$used"
 /usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/used_percentage" -m "$used_percentage"
+/usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/temperature" -m "$temp"
