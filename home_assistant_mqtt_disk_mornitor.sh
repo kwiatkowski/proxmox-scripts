df20f61fa060e22c928ba3949b119a912cc691b9
kwiatkowskimarcin <root@srv.home>
2022-04-05 12:45:46 +0200

    HA mqtt disk monitor

--- home_assistant_mqtt_disk_mornitor.sh
+++ home_assistant_mqtt_disk_mornitor.sh
@@ -4,12 +4,12 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 export $(grep -v '^#' $SCRIPTPATH/.env | xargs -d '\n')
 
-size=`df -h $DISK | awk '{ print $2 }' | tail -n 1 | cut -d 'G' -f1`
-available=`df -h $DISK | awk '{ print $4 }' | tail -n 1 | cut -d 'G' -f1`
-used=`df -h $DISK | awk '{ print $3 }' | tail -n 1 | cut -d 'G' -f1`
-used_percentage=`df -h $DISK | awk '{ print $5 }' | tail -n 1 | cut -d '%' -f1`
+size=`df -h $1 | awk '{ print $2 }' | tail -n 1 | cut -d 'G' -f1`
+available=`df -h $1 | awk '{ print $4 }' | tail -n 1 | cut -d 'G' -f1`
+used=`df -h $1 | awk '{ print $3 }' | tail -n 1 | cut -d 'G' -f1`
+used_percentage=`df -h $1 | awk '{ print $5 }' | tail -n 1 | cut -d '%' -f1`
 
-/usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "monitoring/disk/nas/size" -m "$size"
-/usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "monitoring/disk/nas/available" -m "$available"
-/usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "monitoring/disk/nas/used" -m "$used"
-/usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "monitoring/disk/nas/used_percentage" -m "$used_percentage"
+/usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/size" -m "$size"
+/usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/available" -m "$available"
+/usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/used" -m "$used"
+/usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/used_percentage" -m "$used_percentage"
