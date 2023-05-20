9ce1176c23544cf5ea755bd958b65c9f886834b3
root <root@srv2.local>
2023-05-20 22:43:49 +0200

    Optimization of the disk monitoring script

--- home_assistant_mqtt_processor_monitor.sh
+++ home_assistant_mqtt_processor_monitor.sh
@@ -4,6 +4,6 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 export $(grep -v '^#' $SCRIPTPATH/.env | xargs -d '\n')
 
-temp=`cat /sys/class/thermal/thermal_zone0/temp`
+temp=`sensors --no-adapter 2>/dev/null | grep 'Package id' | awk '{print int($4)}'`
 
 /usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$1/temperature" -m "$temp"
