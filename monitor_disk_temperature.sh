1553b6971f75d3af5e7e4c6eae3186dde72afda9
kwiatkowski <kwiatkowskimarcin.mail@gmail.com>
2023-09-13 20:56:03 +0200

    remove °C nvme0n1

--- monitor_disk_temperature.sh
+++ monitor_disk_temperature.sh
@@ -1,35 +1,14 @@
 #!/bin/sh
 
-SCRIPTPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
-STATEFILE="$SCRIPTPATH/.state"
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-export $(grep -v '^#' "$SCRIPTPATH/.env" | xargs -d '\n')
-
-if [ ! -f "$STATEFILE" ]; then
-    touch "$STATEFILE"
-fi
-
-value_exists() {
-    if grep -q "$1" "$STATEFILE"; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-update_value() {
-    if value_exists "$1"; then
-        sed -i "s|$1=.*|$1=$2|" "$STATEFILE"
-    else
-        echo "$1=$2" >> "$STATEFILE"
-    fi
-}
+export $(grep -v '^#' $SCRIPTPATH/.env | xargs -d '\n')
 
 get_temperature() {
     device=$1
     if echo "$device" | grep -q "/dev/nvme"; then
-        temperature1=$(/usr/sbin/nvme smart-log "$device" | awk '/temperature/ {print $3}')
-        temperature2=$(/usr/sbin/nvme smart-log "$device" | awk '/Temperature Sensor/ {print $5}')
+        temperature1=$(/usr/sbin/nvme smart-log "$device" | awk '/temperature/ {print $3}' | sed 's/°C//')
+        temperature2=$(/usr/sbin/nvme smart-log "$device" | awk '/Temperature Sensor/ {print $5}' | sed 's/°C//')
         echo "$temperature1,$temperature2"
     else
         temperature=$(/usr/sbin/hddtemp -n "$device")
@@ -41,19 +20,9 @@ device=$1
 topic1="$2/temperature"
 topic2="$2/temperature_nvme"
 
-prev_temp1=$(grep "$topic1" "$STATEFILE" | cut -d "=" -f 2 || echo "")
-prev_temp2=$(grep "$topic2" "$STATEFILE" | cut -d "=" -f 2 || echo "")
+temperatures=$(get_temperature "$device")
+temp1=$(echo "$temperatures" | awk -F',' '{print $1}')
+temp2=$(echo "$temperatures" | awk -F',' '{print $2}')
 
-IFS=',' read -r -a temperatures <<< "$(get_temperature "$device")"
-temp1=${temperatures[0]}
-temp2=${temperatures[1]}
-
-if [ "$temp1" != "$prev_temp1" ]; then
-    /usr/bin/mosquitto_pub -h "$MQTT_HOST" -p "$MQTT_PORT" -u "$MQTT_USER" -P "$MQTT_PASS" -t "$topic1" -m "$temp1"
-    update_value "$topic1" "$temp1"
-fi
-
-if [ "$temp2" != "$prev_temp2" ]; then
-    /usr/bin/mosquitto_pub -h "$MQTT_HOST" -p "$MQTT_PORT" -u "$MQTT_USER" -P "$MQTT_PASS" -t "$topic2" -m "$temp2"
-    update_value "$topic2" "$temp2"
-fi
+/usr/bin/mosquitto_pub -h "$MQTT_HOST" -p "$MQTT_PORT" -u "$MQTT_USER" -P "$MQTT_PASS" -t "$topic1" -m "$temp1"
+/usr/bin/mosquitto_pub -h "$MQTT_HOST" -p "$MQTT_PORT" -u "$MQTT_USER" -P "$MQTT_PASS" -t "$topic2" -m "$temp2"
--- monitor_disk_usage.sh
+++ monitor_disk_usage.sh
@@ -1,66 +1,16 @@
 #!/bin/sh
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-STATEFILE="$SCRIPTPATH/.state"
 
 export $(grep -v '^#' $SCRIPTPATH/.env | xargs -d '\n')
 
-if [ ! -f "$STATEFILE" ]; then
-    touch "$STATEFILE"
-fi
-
-# Funkcja sprawdzająca, czy wartość istnieje w pliku stanu
-value_exists() {
-    if grep -q "$1" "$STATEFILE"; then
-        return 0 # Wartość istnieje
-    else
-        return 1 # Wartość nie istnieje
-    fi
-}
-
-# Funkcja aktualizująca wartość w pliku stanu
-update_value() {
-    if value_exists "$1"; then
-        sed -i "s|$1=.*|$1=$2|" "$STATEFILE"
-    else
-        echo "$1=$2" >> "$STATEFILE"
-    fi
-}
-
-# Odczytanie poprzednich wartości z pliku stanu lub ustawienie domyślnych wartości
-prev_size=$(grep "$2/size" "$STATEFILE" | cut -d "=" -f 2 || echo "")
-prev_available=$(grep "$2/available" "$STATEFILE" | cut -d "=" -f 2 || echo "")
-prev_used=$(grep "$2/used" "$STATEFILE" | cut -d "=" -f 2 || echo "")
-prev_used_percentage=$(grep "$2/used_percentage" "$STATEFILE" | cut -d "=" -f 2 || echo "")
-
-# Odczytanie aktualnych wartości
 size=$(df -h "$1" | awk '{ print $2 }' | tail -n 1 | cut -d 'G' -f 1)
 available=$(df -h "$1" | awk '{ print $4 }' | tail -n 1 | cut -d 'G' -f 1)
 used=$(df -h "$1" | awk '{ print $3 }' | tail -n 1 | cut -d 'G' -f 1)
 used_percentage=$(df -h "$1" | awk '{ print $5 }' | tail -n 1 | cut -d '%' -f 1)
 
-# Aktualizacja pliku stanu i wysłanie danych tylko w przypadku zmiany
-if [ "$size" != "$prev_size" ]; then
-    /usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/size" -m "$size"
-    update_value "$2/size" "$size"
-fi
-
-if [ "$available" != "$prev_available" ]; then
-    /usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/available" -m "$available"
-    update_value "$2/available" "$available"
-fi
-
-if [ "$used" != "$prev_used" ]; then
-    /usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/used" -m "$used"
-    update_value "$2/used" "$used"
-fi
-
-if [ "$used_percentage" != "$prev_used_percentage" ]; then
-    /usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/used_percentage" -m "$used_percentage"
-    update_value "$2/used_percentage" "$used_percentage"
-fi
-
-if [ "$temp" != "$prev_temp" ]; then
-    /usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/temperature" -m "$temp"
-    update_value "$2/temperature" "$temp"
-fi
+/usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/size" -m "$size"
+/usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/available" -m "$available"
+/usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/used" -m "$used"
+/usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/used_percentage" -m "$used_percentage"
+/usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t "$2/temperature" -m "$temp"
